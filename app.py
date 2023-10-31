7edf5a23dd3b7a13863be0693ac7bb6dd41f53e7
b-sai <bsaishreyas@gmail.com>
2023-10-31 12:05:44 -0500

    Add title, scores

--- app.py
+++ app.py
@@ -17,13 +17,44 @@ pinecone_index = pinecone.Index(os.environ['IDX_NAME'])
 
 app = Flask(__name__)
 
+def filter_by_threshold(matches, threshold=0.3):
+    """Filter matches by a given threshold."""
+    return [match for match in matches if match.score > threshold]
+
+def extract_metadata(matches):
+    """Extract metadata from matches."""
+    res_lines = []
+    for match in matches:
+        metdat = match.metadata
+        res_line = metdat['BRAND'] if metdat['RETAILER'] == "" else f"{metdat['RETAILER']}, {metdat['BRAND']}"
+        res_lines.append(res_line)
+    return res_lines
+
+def extract_scores(matches):
+    """Extract scores from matches."""
+    return [round(match.score, 2) for match in matches]
+
+def extract_offers(matches):
+    """Extract offer IDs from matches."""
+    return [match.id for match in matches]
+
 def nearest_search(user_input: str):
     res = pinecone_index.query(
-    vector=model.encode([user_input]).tolist(),
-    top_k=3,
-    include_values=True
+        vector=model.encode([user_input]).tolist(),
+        top_k=20,
+        include_metadata=True
     )
-    return [res['matches'][x].id for x in range(len(res['matches']))]
+    
+    # Filter by threshold
+    filtered_matches = filter_by_threshold(res['matches'])
+    
+    # Extract information
+    offers = extract_offers(filtered_matches)
+    res_lines = extract_metadata(filtered_matches)
+    scores = extract_scores(filtered_matches)
+    
+    return offers, res_lines, scores
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -32,12 +63,13 @@ def index():
         query = request.form['search']
         print(query)
         
-        coupons  = nearest_search(query)
+        offers, res_lines, scores  = nearest_search(query)
+        print(scores)
         # Simulated search results
-        search_results = [{'title': f'Result {_+1}', 'content': f'{coupons[_]}'} for _ in range(3)]
+        search_results = [{'title': f'{res_lines[i]}', 'content': f'{offers[i]}','score':f'{scores[i]}'} for i in range(len(scores))]
             # Add more results as needed
        
     return render_template('index.html', search_results=search_results)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)

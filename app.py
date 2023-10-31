3612a9f7dd2ab11dd5d5dd0259f49004184e552f
b-sai <bsaishreyas@gmail.com>
2023-10-31 09:59:10 -0500

    file

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    search_results = []
+    if request.method == 'POST':
+        query = request.form['search']
+        # Simulated search results
+        search_results = [
+            {'title': 'Result 1', 'content': f'Content for {query} 1'},
+            {'title': 'Result 2', 'content': f'Content for {query} 2'},
+            # Add more results as needed
+        ]
+    return render_template('index.html', search_results=search_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
